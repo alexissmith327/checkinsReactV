9cfe63e32f9a43568aa2ad01f2c376a1447838b4
Alexis Smith <alexis@nextlevelurgentcare.com>
2021-07-10 14:31:44 -0500

    checkins1

---
+++ src/components/LocationsNavComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {DropdownItem} from 'reactstrap';
+import {LOCATIONS} from '/Users/alexissmith/Desktop/Checkins React V/src/shared/locations.js'
+
+
+
+
+function RenderLocation({ location }) {
+    return (
+                    <DropdownItem className="nav-link" href={'/directory/'+ location.id}>{location.name}</DropdownItem>
+    );
+}
+
+function LocationNav(props) {
+
+    const location = LOCATIONS.map(location => {
+        return (
+            <div key={location.id}>
+                <RenderLocation location={location} />
+            </div>
+        );
+    });
+
+    return (
+            <div className="DropdownMenu">
+                {location}
+            </div>
+    );
+}
+
+export default LocationNav;
---
+++ src/shared/locations.js
@@ -0,0 +1,102 @@
+export const LOCATIONS = [
+    {
+        id: 0,
+        name: "Baytown",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+    {
+        id: 1,
+        name: "Champions",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 2,
+        name: "Conroe",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 3,
+        name: "Copperfield",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 4,
+        name: "Cypress",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 5,
+        name: "Cinco Ranch",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 6,
+        name: "Falcon Landing",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 7,
+        name: "Katy",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 8,
+        name: "Kingsland",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 9,
+        name: "Long Meadow",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 10,
+        name: "League City",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 11,
+        name: "Meyerland",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 12,
+        name: "Pearland",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 13,
+        name: "Sienna",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 14,
+        name: "Sugar Land",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 15,
+        name: "Tanglewood",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    },
+
+    {
+        id: 16,
+        name: "The Woodlands",
+        image: "/assets/images/NextLevel-Sienna-003.jpg",
+    }
+];
